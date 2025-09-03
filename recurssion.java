56416395fa5cb0aace8a50dc6e3dbe868789026f
Sudhanshu pal <sp7017184@gmail.com>
2025-09-03 23:23:56 +0530

    one day more

---
+++ recurssion.java
@@ -0,0 +1,29 @@
+public class recurssion {
+    public static void reverse(int n , int i){
+        if(n<i){
+            return;
+        }
+
+        System.out.print(n);
+        reverse(n-1, i);
+
+
+
+    }
+
+    // we can do it in another way also in which we can solve that in easy way which contains the both parameters to be same 
+    public static void reverse2(int i , int n){
+        if(i<1) {
+            return; 
+        }
+        System.out.print(i);
+        reverse2( i-1, n);
+    }
+
+    public static void main(String args[]){
+        reverse(5, 01);
+        reverse2(6, 6);
+     
+    }
+    
+}
