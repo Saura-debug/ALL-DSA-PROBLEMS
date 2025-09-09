e41e29a164b7548cd07977710106e8b1ad68b906
Sudhanshu pal <sp7017184@gmail.com>
2025-09-09 22:43:17 +0530

    mergesort

---
+++ mergesorting.java
@@ -0,0 +1,60 @@
+import java.util.ArrayList;
+import java.util.*;
+public class mergesorting {
+
+    public static void sort(int arr[],int low, int high){
+        if(low >= high){
+            return;
+        }
+        int mid = low + (high-low)/2;
+        sort(arr,low,mid);
+
+        sort(arr,mid+1,high);
+        merge(arr,low,mid,high);
+
+    
+    }
+
+
+    public static void merge(int arr[],int low, int mid, int high){
+        int left = low; 
+        int right = mid+1;
+        ArrayList<Integer> aa = new ArrayList<>();
+
+        while(left<=mid && right<= high) {
+            if(arr[left]<=arr[right]){
+                aa.add(arr[left]);
+                left++;
+            }
+            else {
+                aa.add(arr[right]);
+                right++;
+            }
+        }
+
+            while (left<=mid) {
+                aa.add(arr[left]);
+                left++;
+                
+            }
+            while (right<=high) {
+                aa.add(arr[right]);
+                right++;
+                
+            }
+            for(int i= low; i<=high; i++){
+                arr[i] = aa.get(i-low);
+            
+            
+        }
+    }
+    public static void main(String args[]){
+        int arr[] = {4,8,3,9,1,9};
+        int low = 0;
+        int high = arr.length -1;
+        sort(arr,low,high);
+
+        System.out.print(Arrays.toString(arr));
+
+    }
+}
