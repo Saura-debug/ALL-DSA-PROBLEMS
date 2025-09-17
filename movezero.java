6481cc02fb75013fb7497976cd3c8fbdb61b451f
Sudhanshu pal <sp7017184@gmail.com>
2025-09-17 18:12:18 +0530

    added

---
+++ movezero.java
@@ -0,0 +1,46 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+
+public class movezero {
+    public static void main(String args[]){
+        // the bruteforce approch learned by striver with tc = O(n)+O(x)+O(n-x) = O(2n) and space complexity will be O(n)
+            
+        int arr[] = {5,7,0,2,0,4,5,8,0,9,0};
+        // ArrayList<Integer> y = new ArrayList<>();
+        // for(int i =0; i<arr.length; i++){
+        //     if(arr[i]!=0){
+        //         y.add(arr[i]);
+        //     }
+        // }
+        // for(int i =0; i<y.size();i++){
+        //     arr[i] = y.get(i);
+        // }
+
+        // for(int i =y.size();i<arr.length; i++){
+        //     arr[i] = 0;
+        // }
+        // System.out.println(Arrays.toString(arr));
+        int j = -1;
+        int k = 0;
+        // this is the optimal solution for the putting the zero at the last, which have the TC = O(n) + O(n-k) and SC = O(1)
+        for(int i =0; i<arr.length; i++ ){
+            if(arr[i] == 0){
+                j = i;
+                k = i+1;
+                break;
+            }
+
+        }
+
+        for(int p = k; p<arr.length; p++){
+            if(arr[p]!= 0){
+                int temp = arr[p];
+                arr[p] = arr[j];
+                arr[j] = temp;
+                j++;
+            }
+        }
+      System.out.println(Arrays.toString(arr));
+    }
+    
+}
