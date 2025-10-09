e860579ec1fcb9506a715885060eb287446d3b86
Sudhanshu pal <sp7017184@gmail.com>
2025-10-09 14:38:49 +0530

    same

---
+++ sortcolors.java
@@ -0,0 +1,35 @@
+import java.util.Arrays;
+
+public class sortcolors {
+    public static void col(int arr[]){
+        int n = arr.length;
+        int low = 0; 
+        int mid = 0; 
+        int high = n-1;
+        while(mid<=high){
+            if(arr[mid] == 0){
+                int temp = arr[mid];
+                arr[mid] = arr[low];
+                arr[low] = temp;
+                low++;
+            }
+            if(arr[mid]==1){
+                mid++;
+            }
+            if(arr[mid] == 2){
+                int temp = arr[mid];
+                arr[high] = temp;
+                arr[mid] = temp;
+                high--;
+            }
+        }
+        System.out.print(Arrays.toString(arr));
+
+    }
+    public static void main(String args[]){
+        int arr[] = {1,0,0,0,1,0,2,1,0,2,1,0,2,1,0,};
+        col(arr);
+
+    }
+    
+}
