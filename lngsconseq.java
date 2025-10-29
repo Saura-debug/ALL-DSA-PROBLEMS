5c1d25e6d5c979879c892f48a4d970d7a54b17e9
Sudhanshu pal <sp7017184@gmail.com>
2025-10-29 13:36:10 +0530

    solved with the optimal approch

--- lngsconseq.java
+++ lngsconseq.java
@@ -1,4 +1,5 @@
 import java.util.Arrays;
+import java.util.HashSet;
 public class lngsconseq {
 // this code i learned from the striver it was bit difficult
   
@@ -19,10 +20,54 @@ for(int j =0; j<arr.length; j++){
 return lg;
 }
 
+// better solution by striver
+
+// this has time complexity O(n) + O(nlogn)
+private static int lng2(int arr[]){
+  Arrays.sort(arr);
+   int lg = 1;
+   int curr = 0;
+   int lsm = Integer.MIN_VALUE;
+   for(int i =0; i<arr.length; i++){
+    if(arr[i]-1 == lsm){
+      curr++;
+      lsm = arr[i];
+    }
+    else if(arr[i]!=lsm){
+      curr = 1;
+      lsm = arr[i];
+
+    }
+    lg = Math.max(curr,lg);
+   }
+   return lg;
+}
+private static int lng3(int arr[]){
+  // this is the optimal solution given by the striver with TC O(1)  in unsorted HashSet, in collision the Tc can go up to O(n) and SC is O(N)
+  HashSet<Integer> group = new HashSet<>();
+  int lg = 0;
+  int count = 1; 
+  for(int i =0; i<arr.length; i++){
+    group.add(arr[i]);
+  }
+  for(Integer a : group){
+    if(!group.contains(a-1)){
+      count = 1;
+      while (group.contains(a+1)) {
+        count++;
+        a++;
+        
+      }
+    }
+    lg = Math.max(lg, count);
+  }
+  return lg;
+}
+
   
 public static void main(String args[]){
-       int arr[] = {1,0,1,2};
-      System.out.println(lng(arr));
+       int arr[] = {1,0,1,2,3,4};
+      System.out.println(lng3(arr));
        
 
     }
