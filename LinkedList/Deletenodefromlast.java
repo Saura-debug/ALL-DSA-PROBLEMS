fc6bd08d79ccd0c607edaa7044d7d7d7bbc417af
sudhanshu <sp7017184@gmail.com>
2026-03-23 13:01:03 +0530

    solved the Delete N node from last

---
+++ LinkedList/Deletenodefromlast.java
@@ -0,0 +1,2 @@
+//this is the optimal solution for the deleting the node from the last,
+// hear i would  take two varible Fast and slow,at first i would increase the fast n times so that it can have n times step ahead of the slow, after that i will just increase the slow and fast until fast.next gets null  , there i ll endup with the TC O(length of node)
