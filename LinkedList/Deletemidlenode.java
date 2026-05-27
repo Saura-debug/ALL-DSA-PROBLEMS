30406846bf69247cf2702b5544ae404689d22cec
sudhanshu <sp7017184@gmail.com>
2026-05-28 02:45:49 +0530

    Done

---
+++ LinkedList/Deletemidlenode.java
@@ -0,0 +1,9 @@
+public class Deletemidlenode {
+    // it can be solved with two way, first would be bruteforce in which we would first find the middle,
+    // then,we would get the node before the middle and last we would point the previous node to next.next node,
+    // this would take O(n+n/2) time complexity and O(1) space complexity
+    // Now there would be a second way to solve the problem,where will use the hair and tortoise method 
+    // as we know that we ll have the use the of Node before the mid for that we will just increase the fast one step ahead before  increasing  simultoneusly 
+    // and that's it we got our answer 
+    
+}
